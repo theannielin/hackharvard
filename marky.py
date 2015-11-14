6e9734d2722b1175534553db3b7c3c53d1a4fec0
joanne <joannexo@users.noreply.github.com>
2015-11-13 22:28:53 -0500

    scrapy generated questions

---
+++ marky.py
@@ -0,0 +1,16 @@
+import markovify
+
+# Get raw text as string.
+with open("/Users/Joanne/Documents/hackharvard/example.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+# Print five randomly-generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
+# Print three randomly-generated sentences of no more than 140 characters
+for i in range(3):
+    print(text_model.make_short_sentence(140))
