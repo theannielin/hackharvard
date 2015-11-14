858da17f0433f2a806ae6105b3a847d76540fc1d
theannielin <annielin@college.harvard.edu>
2015-11-14 14:29:50 -0500

    links from tags

--- stackoverflow_spider.py
+++ stackoverflow_spider.py
@@ -1,23 +1,18 @@
 import scrapy
 
-
 class StackOverflowSpider(scrapy.Spider):
     name = 'stackoverflow'
-    start_urls = ['http://stackoverflow.com/questions/33389484/advanced-css-tricks-setting-a-column-break-in-css3-multi-column-website-layout', 
-    'http://stackoverflow.com/questions/111102/how-do-javascript-closures-work']
-
-    #def parse(self, response):
-        #for href in response.css('.question-summary h3 a::attr(href)'):
-            #full_url = response.urljoin(href.extract())
-            #yield scrapy.Request(full_url, callback=self.parse_question)
+    start_urls = ['http://stackoverflow.com/questions/tagged/c']
 
     def parse(self, response):
+        for href in response.css('.question-summary h3 a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_question)
+
+    def parse_question(self, response):
         yield {
-            #'title': response.css('h1 a::text').extract()[0],
-            #'votes': response.css('.question .vote-count-post::text').extract()[0],
-            'answer': response.css('.answercell .post-text').extract()[0],
-            #'tags': response.css('.question .post-tag::text').extract(),
-            #'link': response.url,
+            'answer': response.css('.answercell .post-text').extract(),
+            'link': response.url,
         }
 
 
