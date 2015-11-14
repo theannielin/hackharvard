24c766e409f887ed77285ac10edf41e0b6efe5d9
theannielin <annielin@college.harvard.edu>
2015-11-14 15:34:53 -0500

    tmpfile and marky calls spider

--- marky.py
+++ marky.py
@@ -1,30 +1,16 @@
 import markovify
 import scrapy
 import sys
+import os
+import tempfile
 
-input_tag = sys.argv[1]
+# input_tag = sys.argv[1]
 
-class StackOverflowSpider(scrapy.Spider):
-    name = 'stackoverflow'
-    start_urls = ['http://stackoverflow.com/questions/tagged/' + input_tag]
-
-    def parse(self, response):
-        for href in response.css('.question-summary h3 a::attr(href)'):
-            full_url = response.urljoin(href.extract())
-            yield scrapy.Request(full_url, callback=self.parse_question)
-
-    def parse_question(self, response):
-        yield {
-            'title': response.css('h1 a::text').extract()[0],
-            'votes': response.css('.question .vote-count-post::text').extract()[0],
-            'body': response.css('.question .post-text').extract()[0],
-            'tags': response.css('.question .post-tag::text').extract(),
-            'link': response.url,
-        }
-
+from subprocess import call
+call(["scrapy", "runspider", "stackoverflow_spider.py", "-o", "~/Desktop/bobo.json"])
 
 # Get raw text as string.
-with open("/Users/Annie/Dropbox/Fall 2015/Misc/hackharvard/top-stackoverflow-questions.json") as f:
+with open("~/Desktop/bobo.json") as f:
     text = f.read()
 
 # Build the model.
@@ -37,3 +23,5 @@ for i in range(5):
 # Print three randomly-generated sentences of no more than 140 characters
 for i in range(3):
     print(text_model.make_short_sentence(140))
+
+os.remove("~/Desktop/bobo.json")
