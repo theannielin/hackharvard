57adb0c303a2734b6f2148a18cfc580019fec2b4
Nhu Nguyen <hoainhu95@gmail.com>
2015-11-14 14:04:13 -0500

    getting answers

--- stackoverflow_spider.py
+++ stackoverflow_spider.py
@@ -3,18 +3,18 @@ import scrapy
 
 class StackOverflowSpider(scrapy.Spider):
     name = 'stackoverflow'
-    start_urls = ['http://stackoverflow.com/questions/tagged/' + input_tag]
+    start_urls = ['http://stackoverflow.com/questions/33389484/advanced-css-tricks-setting-a-column-break-in-css3-multi-column-website-layout']
+
+    #def parse(self, response):
+        #for href in response.css('.question-summary h3 a::attr(href)'):
+            #full_url = response.urljoin(href.extract())
+            #yield scrapy.Request(full_url, callback=self.parse_question)
 
     def parse(self, response):
-        for href in response.css('.question-summary h3 a::attr(href)'):
-            full_url = response.urljoin(href.extract())
-            yield scrapy.Request(full_url, callback=self.parse_question)
-
-    def parse_question(self, response):
         yield {
-            'title': response.css('h1 a::text').extract()[0],
-            'votes': response.css('.question .vote-count-post::text').extract()[0],
-            'body': response.css('.question .post-text').extract()[0],
-            'tags': response.css('.question .post-tag::text').extract(),
-            'link': response.url,
+            #'title': response.css('h1 a::text').extract()[0],
+            #'votes': response.css('.question .vote-count-post::text').extract()[0],
+            'answer': response.css('#answer-33668329 .post-text').extract()[0],
+            #'tags': response.css('.question .post-tag::text').extract(),
+            #'link': response.url,
         }
