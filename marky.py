ee584f595438e56af3e643372370dedcdc454871
theannielin <annielin@college.harvard.edu>
2015-11-13 21:08:24 -0500

    started

---
+++ marky.py
@@ -0,0 +1,16 @@
+import markovify
+
+# Get raw text as string.
+with open("/Users/Annie/Dropbox/Fall 2015/Misc/hackharvard/gogo.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+# Print five randomly-generated sentences
+for i in range(5):
+    print(text_model.make_sentence())
+
+# Print three randomly-generated sentences of no more than 140 characters
+for i in range(3):
+    print(text_model.make_short_sentence(140))
