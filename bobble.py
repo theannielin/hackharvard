3bd1f56f1cb631f31d27c994bea3ba8ed9f10f85
joanne <joannexo@users.noreply.github.com>
2015-11-14 21:46:34 -0500

    integrated into server

--- bobble.py
+++ bobble.py
@@ -1,8 +1,19 @@
-from bottle import route, run
+from bottle import route, run, template, static_file
 from marky import marky
+
+
+@route('/static/<filename>')
+def static(filename):
+    return static_file(filename, root='/Users/Joanne/Documents/hackharvard/static')
+
+@route('/')
+def home(): 
+	return template('home.html')
 
 @route('/marks')
 def marks():
     return marky(5, 'ocaml')
 
 run(host='localhost', port=8080, debug=True)
+
+
