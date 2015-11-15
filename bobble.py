61d6984960e04bc4393cbbed491d586a166d50f8
theannielin <annielin@college.harvard.edu>
2015-11-14 21:35:04 -0500

    json bobble

--- bobble.py
+++ bobble.py
@@ -1,8 +1,11 @@
 from bottle import route, run
 from marky import marky
+import json
 
 @route('/marks')
 def marks():
-    return marky(5, 'ocaml')
+	answer = {}
+	answer['answer'] = marky(5, 'ocaml')
+	return json.dumps(answer)
 
 run(host='localhost', port=8080, debug=True)
