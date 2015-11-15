9d2765bdb9da8c3c94f9ebf63213e3b79e686195
Nhu Nguyen <hoainhu95@gmail.com>
2015-11-14 22:58:01 -0500

    bottle on heroku

--- bobble.py
+++ bobble.py
@@ -18,6 +18,6 @@ def marks(sentences, tag):
 	answer['answer'] = marky(sentences, tag)
 	return json.dumps(answer)
 
-run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
