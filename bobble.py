5c8899cb4602057df01cee5c6af2c659186139ea
Nhu Nguyen <hoainhu95@gmail.com>
2015-11-14 23:34:56 -0500

    really getting on

--- bobble.py
+++ bobble.py
@@ -18,6 +18,6 @@ def marks(sentences, tag):
 	answer['answer'] = marky(sentences, tag)
 	return json.dumps(answer)
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(host="hackharvard.herokuapp.com")
 
 
