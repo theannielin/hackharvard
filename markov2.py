ab6c077ba0a78e864d9345edcd6156177e9db93f
theannielin <annielin@college.harvard.edu>
2015-11-29 11:04:26 -0500

    added class

--- markov2.py
+++ markov2.py
@@ -1,33 +1,39 @@
 import random
 
-def Markov(text_file):
+class Markov():
+    def __init__(self):
+        self.markov = []
 
-    with open(text_file) as f:    # provide a text-file to parse
-        data = f.read()
+    def parse_text(self, text_file):
+        with open(text_file) as f:    # provide a text-file to parse
+            data = f.read()
+        data = [i for i in data.split(' ') if i != '']     # create a list of all words 
+        data = [i.lower() for i in data if i.isalpha()]    # i've been removing punctuation
+        self.markov = {i:[] for i in data}    # i create a dict with the words as keys and empty lists as values
 
-    data = [i for i in data.split(' ') if i != '']     # create a list of all words 
-    data = [i.lower() for i in data if i.isalpha()]    # i've been removing punctuation
+        pos = 0
+        while pos < len(data) - 1:    # add a word to the word-key's list if it immediately follows that word
+            self.markov[data[pos]].append(data[pos+1])
+            pos += 1
 
-    markov = {i:[] for i in data}    # i create a dict with the words as keys and empty lists as values
+    def seeding(self):
+        new = {k:v for k,v in zip(range(len(self.markov)), [i for i in self.markov])}    # create another dict for the seed to match up with 
 
-    pos = 0
-    while pos < len(data) - 1:    # add a word to the word-key's list if it immediately follows that word
-        markov[data[pos]].append(data[pos+1])
-        pos += 1
+        length_sentence = random.randint(15, 20)    # create a random length for a sentence stopping point
 
-    new = {k:v for k,v in zip(range(len(markov)), [i for i in markov])}    # create another dict for the seed to match up with 
+        seed = random.randint(0, len(new) - 1)    # randomly pick a starting point
 
-    length_sentence = random.randint(15, 20)    # create a random length for a sentence stopping point
+        sentence_data = [new[seed]]     # use that word as the first word and starting point
+        current_word = new[seed]
 
-    seed = random.randint(0, len(new) - 1)    # randomly pick a starting point
+        while len(sentence_data) < length_sentence:
+            next_index = random.randint(0, len(self.markov[current_word]) - 1)    # randomly pick a word from the last words list.
+            next_word = self.markov[current_word][next_index]
+            sentence_data.append(next_word)
+            current_word = next_word
 
-    sentence_data = [new[seed]]     # use that word as the first word and starting point
-    current_word = new[seed]
+        return ' '.join([i for i in sentence_data])
 
-    while len(sentence_data) < length_sentence:
-        next_index = random.randint(0, len(markov[current_word]) - 1)    # randomly pick a word from the last words list.
-        next_word = markov[current_word][next_index]
-        sentence_data.append(next_word)
-        current_word = next_word
-
-    return ' '.join([i for i in sentence_data])
+    def marking(self, text_file):
+        self.parse_text(text_file)
+        return self.seeding()
