7caeff4ca2c925e0284d53ba8afd3df118249d58
joanne <joannexo@users.noreply.github.com>
2015-12-03 22:35:58 -0500

    tests implemented

--- markov.py
+++ markov.py
@@ -26,12 +26,47 @@ class Search():
         self.parse_text(text_file)
         return self.response()
 
-class Weighting():
+class MarkovWeighting():
     def __init__(self):
-        self.data = []
+        self.markov = []
 
     def parse_text(self, text_file):
-        data = []
+        with open(text_file) as f:    # provide a text-file to parse
+            data = f.read()
+        data = [i for i in data.split(' ') if i != '']     # create a list of all words 
+        data = [i.lower() for i in data if i.isalpha()]    # i've been removing punctuation
+        self.markov = {i:[] for i in data}    # i create a dict with the words as keys and empty lists as values
+
+        pos = 0
+        while pos < len(data) - 1:    # add a word to the word-key's list if it immediately follows that word
+            self.markov[data[pos]].append(data[pos+1])
+            pos += 1
+
+    def seeding(self):
+        new = {k:v for k,v in zip(range(len(self.markov)), [i for i in self.markov])}    # create another dict for the seed to match up with 
+
+        length_sentence = random.randint(15, 20)    # create a random length for a sentence stopping point
+
+        seed = random.randint(0, len(new) - 1)    # randomly pick a starting point
+
+        sentence_data = [new[seed]]     # use that word as the first word and starting point
+        current_word = new[seed]
+
+        while len(sentence_data) < length_sentence:
+            if len(self.markov[current_word]) == 0: 
+                self.seeding()
+            next_index = random.randint(0, len(self.markov[current_word]) - 1)    # randomly pick a word from the last words list.
+            next_word = self.markov[current_word][next_index]
+            sentence_data.append(next_word)
+            current_word = next_word
+
+        return ' '.join([i for i in sentence_data])
+
+    def marking(self, text_file):
+        self.parse_text(text_file)
+        return self.seeding()
+
+
 
 class SDP():
     def __init__(self):
--- test.py
+++ test.py
@@ -46,7 +46,70 @@ def search(num_sentences, tag):
     os.remove("~/Desktop/bobo.json")
     return sentences
 
+
+
+def markovWeighting(num_sentences, tag):
+    if os.path.isfile("~/Desktop/bobo.json"):
+        os.remove("~/Desktop/bobo.json")
+
+    call(["scrapy", "runspider", "stackoverflow_spider.py", "-a", "tag=%s" %tag ,"-o", "~/Desktop/bobo.json"], 
+         stdout=devnull, stderr=devnull)
+
+    with open("~/Desktop/bobo.json") as f:
+        text = f.read()
+
+    sentences = ''
+
+    # Print sentences from random answers related to tag
+    for i in range(num_sentences): 
+        s = markov.Markov()
+        ans = str(s.marking("~/Desktop/bobo.json"))
+        sent_list = ans.split('.')
+        rando = random.randint(0, len(sent_list) - 1)
+        sentence = sent_list[rando]
+        if ans:
+            # remove too many spaces
+            sentence = " ".join(sentence.split())
+            sentences += sentence + '. '
+        else:
+            sentences += 'Recompile.'
+
+    os.remove("~/Desktop/bobo.json")
+    return sentences
+
+
+def mmarkov(num_sentences, tag):
+    if os.path.isfile("~/Desktop/bobo.json"):
+        os.remove("~/Desktop/bobo.json")
+
+    call(["scrapy", "runspider", "stackoverflow_spider.py", "-a", "tag=%s" %tag ,"-o", "~/Desktop/bobo.json"], 
+         stdout=devnull, stderr=devnull)
+
+    with open("~/Desktop/bobo.json") as f:
+        text = f.read()
+
+    sentences = ''
+
+    # Print sentences from random answers related to tag
+    for i in range(num_sentences): 
+        s = markov.MarkovWeighting()
+        ans = str(s.marking("~/Desktop/bobo.json"))
+        sent_list = ans.split('.')
+        rando = random.randint(0, len(sent_list) - 1)
+        sentence = sent_list[rando]
+        if ans:
+            # remove too many spaces
+            sentence = " ".join(sentence.split())
+            sentences += sentence + '. '
+        else:
+            sentences += 'Recompile.'
+
+    os.remove("~/Desktop/bobo.json")
+    return sentences
+
 if __name__ == "__main__":
     sentences = int(input('Number of sentences: '))
     tag = str(input('Tag (between quotes): '))
     print "1. SEARCH: " + search(sentences, tag)
+    print "2. Markov: " + mmarkov(sentences, tag)
+    print "3. Markov Weighting: " + markovWeighting(sentences, tag)
